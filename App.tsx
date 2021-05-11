cb5f79ad96f1969dfbfd229ca59d2513b28ddaa0
thoriqdharmawan <thoriqd21@gmail.com>
2021-05-11 22:41:18 +0700

    feat: intial commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,123 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import {
+  StatusBar,
+  Image,
+  Animated,
+  Text,
+  View,
+  StyleSheet,
+} from "react-native";
+import faker from "faker";
 
-export default function App() {
+faker.seed(10);
+const DATA = [...Array(30).keys()].map((_, i) => {
+  return {
+    key: faker.random.uuid(),
+    image: `https://randomuser.me/api/portraits/${faker.helpers.randomize([
+      "women",
+      "men",
+    ])}/${faker.random.number(60)}.jpg`,
+    name: faker.name.findName(),
+    jobTitle: faker.name.jobTitle(),
+    email: faker.internet.email(),
+  };
+});
+
+const BG_IMG =
+  "https://images.pexels.com/photos/1231265/pexels-photo-1231265.jpeg";
+const SPACING = 20;
+const AVATAR_SIZE = 70;
+const ITEM_SIZE = AVATAR_SIZE + SPACING * 3;
+
+export default () => {
+  const scrollY = React.useRef(new Animated.Value(0)).current;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "#fff" }}>
+      <Image
+        source={{ uri: BG_IMG }}
+        style={StyleSheet.absoluteFill}
+        blurRadius={80}
+      />
+      <Animated.FlatList
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+          { useNativeDriver: true }
+        )}
+        data={DATA}
+        keyExtractor={(item) => item.key}
+        contentContainerStyle={{
+          padding: SPACING,
+          paddingTop: StatusBar.currentHeight || 42,
+        }}
+        renderItem={({ item, index }) => {
+          const inputRange = [
+            -1,
+            0,
+            ITEM_SIZE * index,
+            ITEM_SIZE * (index + 2),
+          ];
+
+          const opacityInputRange = [
+            -1,
+            0,
+            ITEM_SIZE * index,
+            ITEM_SIZE * (index + .5),
+          ];
+
+          const scale = scrollY.interpolate({
+            inputRange,
+            outputRange: [1, 1, 1, 0],
+          });
+
+          const opacity = scrollY.interpolate({
+            inputRange: opacityInputRange,
+            outputRange: [1, 1, 1, 0],
+          });
+
+          return (
+            <Animated.View
+              style={{
+                flexDirection: "row",
+                padding: SPACING,
+                borderRadius: 12,
+                marginBottom: SPACING,
+                shadowColor: "#000",
+                shadowOffset: {
+                  width: 0,
+                  height: 10,
+                },
+                opacity,
+                shadowOpacity: 0.3,
+                shadowRadius: 20,
+                transform: [{ scale }],
+              }}
+            >
+              <Image
+                source={{ uri: item.image }}
+                style={{
+                  width: AVATAR_SIZE,
+                  height: AVATAR_SIZE,
+                  borderRadius: AVATAR_SIZE,
+                  marginRight: SPACING / 2,
+                }}
+              />
+              <View>
+                <Text style={{ fontSize: 22, fontWeight: "700" }}>
+                  {item.name}
+                </Text>
+                <Text style={{ fontSize: 18, opacity: 0.7 }}>
+                  {item.jobTitle}
+                </Text>
+                <Text style={{ fontSize: 14, opacity: 0.8, color: "#0099cc" }}>
+                  {item.email}
+                </Text>
+              </View>
+            </Animated.View>
+          );
+        }}
+      />
+      <StatusBar hidden />
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
